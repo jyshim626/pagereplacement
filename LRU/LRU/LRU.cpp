7bcc0eddad3d02393e902074e1f3d1f77eda25b9
jyshim626 <jyshim626@naver.com>
2017-11-23 20:35:22 +0900

    커밋 2

--- LRU/LRU/LRU.cpp
+++ LRU/LRU/LRU.cpp
@@ -1,11 +1,14 @@
 #include "stdafx.h"
 #include <stdio.h>
 
-int findLRU(int time[], int n) {
+int findLRU(int time[], int n) 
+{
 	int i, minimum = time[0], pos = 0;
 
-	for (i = 1; i < n; ++i) {
-		if (time[i] < minimum) {
+	for (i = 1; i < n; ++i) 
+	{
+		if (time[i] < minimum) 
+		{
 			minimum = time[i];
 			pos = i;
 		}
@@ -25,19 +28,24 @@ int main()
 
 	printf("Enter reference string: ");
 
-	for (i = 0; i < no_of_pages; ++i) {
+	for (i = 0; i < no_of_pages; ++i) 
+	{
 		scanf_s("%d", &pages[i]);
 	}
 
-	for (i = 0; i < no_of_frames; ++i) {
+	for (i = 0; i < no_of_frames; ++i) 
+	{
 		frames[i] = -1;
 	}
 
-	for (i = 0; i < no_of_pages; ++i) {
+	for (i = 0; i < no_of_pages; ++i) 
+	{
 		flag1 = flag2 = 0;
 
-		for (j = 0; j < no_of_frames; ++j) {
-			if (frames[j] == pages[i]) {
+		for (j = 0; j < no_of_frames; ++j) 
+		{
+			if (frames[j] == pages[i]) 
+			{
 				counter++;
 				time[j] = counter;
 				flag1 = flag2 = 1;
@@ -45,9 +53,12 @@ int main()
 			}
 		}
 
-		if (flag1 == 0) {
-			for (j = 0; j < no_of_frames; ++j) {
-				if (frames[j] == -1) {
+		if (flag1 == 0) 
+		{
+			for (j = 0; j < no_of_frames; ++j) 
+			{
+				if (frames[j] == -1)
+				{
 					counter++;
 					faults++;
 					frames[j] = pages[i];
@@ -58,7 +69,8 @@ int main()
 			}
 		}
 
-		if (flag2 == 0) {
+		if (flag2 == 0)
+		{
 			pos = findLRU(time, no_of_frames);
 			counter++;
 			faults++;
@@ -68,7 +80,8 @@ int main()
 
 		printf("\n");
 
-		for (j = 0; j < no_of_frames; ++j) {
+		for (j = 0; j < no_of_frames; ++j) 
+		{
 			printf("%d\t", frames[j]);
 		}
 	}
